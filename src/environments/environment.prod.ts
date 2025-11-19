66188b6e83ca4dbbc643adf8935abf6e39bdadf0
Christian Mendez <3951612+cmendez@users.noreply.github.com>
2025-11-19 11:33:37 -0500

    Cambios para desplegar en Vercel

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  // Tu backend PHP en Render (Slim)
+  phpApiUrl: 'https://upch-slim-php-realworld.onrender.com/api',
+  
+  // Tu backend Python en Render (FastAPI)
+  pythonApiUrl: 'https://upch-2do-backend-python-realworld.onrender.com/api'
+};
