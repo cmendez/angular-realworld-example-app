e7ca35c7d9f1d3128cc46a7feebd1fe3e3510a4c
Christian Mendez <3951612+cmendez@users.noreply.github.com>
2025-11-19 11:59:00 -0500

    Config: URLs dinámicas (Local vs Render) en environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,20 @@
+// Detectamos si el navegador está corriendo en localhost o 127.0.0.1
+const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+
 export const environment = {
-  production: false,
-  // Renombrado para claridad
-  phpApiUrl: 'http://localhost:8000/api', 
-  // Añadimos el nuevo backend de Python
-  pythonApiUrl: 'http://localhost:8080/api' 
+  // Si es local, production es false. Si es nube, es true.
+  production: !isLocal,
+
+  // LÓGICA DINÁMICA:
+  // ¿Estoy en local? -> Usa localhost:8000
+  // ¿Estoy en Vercel? -> Usa Render PHP
+  phpApiUrl: isLocal 
+    ? 'http://localhost:8000/api' 
+    : 'https://upch-slim-php-realworld.onrender.com/api',
+
+  // ¿Estoy en local? -> Usa localhost:8080
+  // ¿Estoy en Vercel? -> Usa Render Python
+  pythonApiUrl: isLocal 
+    ? 'http://localhost:8080/api' 
+    : 'https://upch-2do-backend-python-realworld.onrender.com/api'
 };
